2b2758d5f52b376b6e60ecbc089e9935ebec1904
Jeremy List <quick.dudley@gmail.com>
2018-05-25 10:35:10 +1200

    Add `tab split` vim command

--- yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/Tab.hs
+++ yi-keymap-vim/src/Yi/Keymap/Vim/Ex/Commands/Tab.hs
@@ -10,7 +10,8 @@ module Yi.Keymap.Vim.Ex.Commands.Tab (parse) where
 
 import           Control.Applicative              (Alternative(..))
 import           Control.Monad                    (void)
-import qualified Data.Attoparsec.Text as P        (anyChar, char, string)
+import           Control.Monad.State.Class        (get)
+import qualified Data.Attoparsec.Text as P
 import           Data.List                        (null)
 import qualified Data.Text as T                   (pack)
 
@@ -18,14 +19,22 @@ import Yi.Keymap                                  (Action(EditorA))
 import           Yi.Keymap.Vim.Common             (EventString)
 import qualified Yi.Keymap.Vim.Ex.Commands.Common as Common (parse, pureExCommand)
 import           Yi.Keymap.Vim.Ex.Types           (ExCommand(cmdAction, cmdShow))
-import           Yi.Editor                        (printMsg)
+import           Yi.Editor                        (currentBuffer,newTabE,printMsg,switchToBufferE)
 
 parse :: EventString -> Maybe ExCommand
 parse = Common.parse $ do
   void $ P.string "tab"
-  n <- (some (P.char ' ') *> many (P.anyChar))
+  parseTabSplit
+
+parseTabSplit :: P.Parser ExCommand
+parseTabSplit = do
+  void $ P.many1 P.space
+  P.string "split"
   return $ Common.pureExCommand {
-    cmdShow = mappend "tab " (T.pack n),
-	cmdAction = EditorA $ do
-	  printMsg "Not yet implemented!"
+    cmdShow = "tab split",
+    cmdAction = EditorA $ do
+      e <- get
+      let br = currentBuffer e
+      newTabE
+      switchToBufferE br
    }
